e6181029971e3b9ea1e19fceaddf9fbd1f5cb68c
romanbrancato <89923118+romanbrancato@users.noreply.github.com>
2023-07-02 17:04:25 -0400

    Fixed bot getting stuck in certain situations and gui progress

--- bot.py
+++ bot.py
@@ -4,18 +4,19 @@ from detection.image_rec import *
 
 
 class Bot:
-    def __init__(self, g, ss, gLimit, ssLimit, bmTarget, mmTarget, client):
+    def __init__(self, client, g, ss, g_limit, ss_limit, bm_target, mm_target):
+        self.client = client  # Emulator instance
         self.g = g  # Current gold
         self.ss = ss  # Current skystones
-        self.ssLimit = ssLimit  # Skystone limit
-        self.gLimit = gLimit  # Gold limit
-        self.bmTarget = bmTarget  # Bookmark target
-        self.mmTarget = mmTarget  # Mystic medal target
-        self.client = client  # Emulator instance
+        self.gLimit = g_limit  # Gold limit
+        self.ssLimit = ss_limit  # Skystone limit
+        self.bmTarget = bm_target  # Bookmark target
+        self.mmTarget = mm_target  # Mystic medal target
 
         self.bm = 0  # Number of bookmarks acquired
         self.mm = 0  # Number of mystic medals acquired
         self.refreshes = 0  # Number of refreshes done
+        self.bought_currencies = {"bm": False, "mm": False}  # Track bought currencies
 
     def can_refresh(self):
         if self.bm < self.bmTarget or self.mm < self.mmTarget:
@@ -30,8 +31,10 @@ class Bot:
 
     def handle_refresh(self, scrolled=False):
         if self.can_refresh():
-            self.buy_currency("bm", 5)
-            self.buy_currency("mm", 50)
+            if not self.bought_currencies["bm"]:
+                self.buy_currency("bm", 5)
+            if not self.bought_currencies["mm"]:
+                self.buy_currency("mm", 50)
 
             if scrolled:
                 self.perform_refresh()
@@ -42,11 +45,13 @@ class Bot:
                 self.handle_refresh(scrolled=True)
 
     def buy_currency(self, currency_type, quantity, attempts=3):
+
         image_name = f"buy_{currency_type}.png"
         self.client.capture_screen()
         currency = locate_image(f"{currency_type}.png", self.client.index)
         if currency:
-            self.client.click_on_location((currency[0] + 415, currency[1] + 20))  # Location of buy button relative to bm/mm image
+            self.client.click_on_location(
+                (currency[0] + 415, currency[1] + 20))  # Location of buy button relative to bm/mm image
             buy_button = locate_image(image_name, self.client.index)
             counter = 0
 
@@ -62,13 +67,15 @@ class Bot:
                 self.buy_currency(currency_type, quantity, attempts)
             else:
                 self.client.click_on_location(buy_button)
-                self.g -= 184000 if currency_type == "bm" else 280000 # Subtract currency amount after purchase
+                self.g -= 184000 if currency_type == "bm" else 280000  # Subtract currency amount after purchase
                 sleep(1)
 
                 if currency_type == "bm":
                     self.bm += quantity
                 elif currency_type == "mm":
                     self.mm += quantity
+
+                self.bought_currencies[currency_type] = True
 
     def perform_refresh(self, attempts=3):
         self.client.click_on_location((162, 494))  # Refresh button location
@@ -90,4 +97,5 @@ class Bot:
             self.client.click_on_location(confirm_button)
             self.refreshes += 1
             self.ss -= 3
+            self.bought_currencies = {"bm": False, "mm": False}  # Reset bought currencies
             sleep(1)
--- gui.py
+++ gui.py
@@ -1,25 +1,42 @@
-
 import customtkinter
 
-customtkinter.set_appearance_mode("dark")
-customtkinter.set_default_color_theme("green")
+from bot import Bot
+from client import Client
 
-app = customtkinter.CTk()
-app.title("e7 Refresh Bot")
 
-table_frame = customtkinter.CTkFrame(app)
-table_frame.pack(fill=customtkinter.BOTH, expand=True)
+class App:
+    def __init__(self):
+        customtkinter.set_appearance_mode("dark")
+        customtkinter.set_default_color_theme("green")
+
+        self.app = customtkinter.CTk()
+        self.app.title("e7 Refresh Bot")
+
+        self.table_frame = customtkinter.CTkFrame(self.app)
+        self.table_frame.pack(fill=customtkinter.BOTH, expand=True)
+        self.table = Table(self.table_frame, self)
+
+        self.update_geometry()
+
+        self.app.mainloop()
+
+    def update_geometry(self):
+        self.app.update_idletasks()
+        width = self.app.winfo_reqwidth()
+        height = self.app.winfo_reqheight()
+        self.app.geometry(f"{width}x{height}")
 
 
 class Table:
-    def __init__(self, parent_frame):
+    def __init__(self, parent_frame, app):
         self.parent_frame = parent_frame
+        self.app = app
         self.column_labels = [
             "Index",
-            "Gold Limit",
-            "SS Limit",
-            "BM/BM Target (Rate)",
-            "MM/MM Target (Rate)",
+            "Gold/Limit",
+            "SS/Limit",
+            "BM/Target (Rate)",
+            "MM/Target (Rate)",
             "Refreshes"
         ]
         self.rows = []
@@ -30,33 +47,46 @@ class Table:
             label.grid(row=0, column=i + 1, padx=5, sticky="w")
         self.create_last_row()
 
-    def add_row(self):
+    def add_emulator(self):
+        # Get the values from the entry fields
+        entry_values = [entry.get() for entry in self.rows[:] if
+                        isinstance(entry, customtkinter.CTkEntry)]
+
+        # Assign values to variables
+        index, gold_limit, ss_limit, bm_target, mm_target = entry_values[-5:]
+        gold_amount, gold_limit = gold_limit.split("/")
+        ss_amount, ss_limit = ss_limit.split("/")
+
+        # Create client and bot instances
+        client = Client(index)
+        bot = Bot(client, int(gold_amount), int(ss_amount), int(gold_limit), int(ss_limit), int(bm_target),
+                  int(mm_target))
+
         # Create a new row
-        new_row = Row(self.parent_frame, self.row_counter)
+        new_row = Row(self.parent_frame, self.row_counter, bot)
         self.rows.append(new_row)
         self.row_counter = len(self.rows)
+
+        new_row.update_labels([index, gold_limit, ss_limit, bm_target, mm_target])
 
         # Move the contents of the last row down
         for i, row_item in enumerate(self.rows[:6]):
             row_item.grid(row=self.row_counter)
 
         # Update the app's geometry to accommodate the new row
-        app.update_idletasks()  # Wait for all pending tasks to complete
-        width = self.parent_frame.winfo_reqwidth()
-        height = self.parent_frame.winfo_reqheight()
-        app.geometry(f"{width}x{height}")
+        self.app.update_geometry()
 
     def create_last_row(self):
-        add_emulator_button = customtkinter.CTkButton(self.parent_frame, text="Add Emulator", command=self.add_row)
+        add_emulator_button = customtkinter.CTkButton(self.parent_frame, text="Add Emulator", command=self.add_emulator)
         add_emulator_button.grid(row=self.row_counter, column=7, padx=5, pady=5)
         self.rows.append(add_emulator_button)
 
         entry_text = [
             "Index",
-            "Gold Limit",
-            "SS Limit",
+            "Gold/Limit",
+            "SS/Limit",
             "BM Target",
-            "MM Target",
+            "MM Target"
         ]
         for i, text in enumerate(entry_text):
             entry = customtkinter.CTkEntry(self.parent_frame, placeholder_text=text)
@@ -65,9 +95,10 @@ class Table:
 
 
 class Row:
-    def __init__(self, parent_frame, row_number):
+    def __init__(self, parent_frame, row_number, bot):
         self.parent_frame = parent_frame
         self.row_number = row_number
+        self.bot = bot
         self.columns = []
 
         self.create_columns()
@@ -75,7 +106,8 @@ class Row:
     def create_columns(self):
         for i in range(8):
             if i == 7:
-                delete = customtkinter.CTkButton(self.parent_frame, text="X", fg_color="red", hover_color="maroon", width=30, command=self.delete)
+                delete = customtkinter.CTkButton(self.parent_frame, text="X", fg_color="transparent",
+                                                 hover_color="maroon", width=30, command=self.delete)
                 delete.grid(row=self.row_number, column=i + 1, padx=5, pady=5)
                 self.columns.append(delete)
             elif i == 6:
@@ -83,23 +115,21 @@ class Row:
                 button.grid(row=self.row_number, column=i + 1, padx=5, pady=5)
                 self.columns.append(button)
             else:
-                label = customtkinter.CTkLabel(self.parent_frame, text="000")
+                label = customtkinter.CTkLabel(self.parent_frame, text="0")
                 label.grid(row=self.row_number, column=i + 1, padx=5, pady=5, sticky="w")
                 self.columns.append(label)
 
     def refresh(self):
-        pass
+        self.bot.handle_refresh()
 
     def delete(self):
         pass
 
+    def update_labels(self, values):
+        for i, value in enumerate(values):
+            if i < len(self.columns) - 2:
+                label = self.columns[i]
+                label.configure(text=value)
 
-table = Table(table_frame)
 
-# Set the initial app's geometry based on the table frame's size
-app.update()
-width = table_frame.winfo_width()
-height = table_frame.winfo_height()
-app.geometry(f"{width}x{height}")
-
-app.mainloop()
+app = App()
